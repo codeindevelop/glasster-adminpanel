5064541b5485533be3b8f06e52965284a4995d3a
codeindevelopment <codeindevelopment@gmail.com>
2022-03-08 23:12:17 +0330

    change page title dynamicly

--- src/@core/layout/MainLayout.jsx
+++ src/@core/layout/MainLayout.jsx
@@ -4,9 +4,11 @@ import { useSelector } from 'react-redux';
 export default function MainLayout({ children }) {
   const { dir } = useSelector((state) => ({
     dir: state.layout.config.direction,
+    dir: state.layout.config.direction,
   }));
 
   useEffect(() => {
+    document.title = 'Glasster';
     if (dir === 'rtl') {
       document.getElementsByTagName('html')[0].setAttribute('dir', 'rtl');
     } else {
