86baac818c2c1419a87c0b3ecebed4b9cd96baa8
codeindevelopment <codeindevelopment@gmail.com>
2022-03-08 22:53:46 +0330

    implement redux persist config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,15 +11,17 @@ import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
 import './@core/assets/scss/_persian-fonts.scss';
 
 // Redux Config Files
-import store from './redux/store';
+import store, { persistor } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/rootReducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
 const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const persistedReducer = persistReducer(persistConfig, rootReducer, initialState);
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middleware)));
+export const persistor = persistStore(store);
 
 export default store;
