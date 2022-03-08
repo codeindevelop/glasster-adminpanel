712752104ca4160d36cdf8e03680a70617750719
codeindevelopment <codeindevelopment@gmail.com>
2022-03-08 19:10:29 +0330

    implement core colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,32 @@
+const defaultcolors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['iransans'],
+      pelak: ['pelak'],
+      yekan: ['yekanbakh'],
+      dana: ['dana'],
+    },
+    colors: {
+      ...defaultcolors,
+      // Light Colors
+      'light-primary': '#f1faff',
+      'light-danger': '#fff5f8',
+      'light-info': '#f8f5ff',
+      'light-success': '#e8fff3',
+      'light-warning': '#fff8dd',
+
+      //Core Colors
+      primary: '#009ef7',
+      success: '#50cd89',
+      info: '#7239ea',
+      warning: '#ffc700',
+      danger: '#f1416c',
+      dark: '181c32',
+    },
     extend: {},
   },
   plugins: [],
