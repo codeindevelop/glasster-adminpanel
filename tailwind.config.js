0cd429ef0e732791a1af616a952e0aec66e8a678
codeindevelopment <codeindevelopment@gmail.com>
2022-03-12 22:24:31 +0330

    modify tailwindcss and implement some config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 const defaultcolors = require('tailwindcss/colors');
+let colors = require('tailwindcss/colors');
+colors = { ...colors, ...{ transparent: 'transparent' } };
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -13,10 +15,33 @@ module.exports = {
       dana: ['dana'],
       // English Fonts
       pop: ['Poppins Regular'],
+      'pop-light': ['Poppins Light'],
+      'pop-med': ['Poppins Medium'],
+      'pop-bold': ['Poppins Bold'],
       sf: ['sf-ui'],
     },
+    fontSize: {
+      xxs: ['0.6rem', { lineHeight: '1.2' }],
+      xs: ['0.75rem', { lineHeight: '1.5' }],
+      sm: ['0.875rem', { lineHeight: '1.5715' }],
+      base: ['1rem', { lineHeight: '1.5' }],
+      lg: ['1.125rem', { lineHeight: '1.5' }],
+      xl: ['1.25rem', { lineHeight: '1.5' }],
+      '2xl': ['1.5rem', { lineHeight: '1.33' }],
+      '3xl': ['1.88rem', { lineHeight: '1.33' }],
+      '4xl': ['2.25rem', { lineHeight: '1.25' }],
+      '5xl': ['3rem', { lineHeight: '1.25' }],
+      '6xl': ['3.75rem', { lineHeight: '1.2' }],
+    },
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     colors: {
-      ...defaultcolors,
+      ...colors,
       // Light Colors
       'light-primary': '#f1faff',
       'light-danger': '#fff5f8',
@@ -32,7 +57,18 @@ module.exports = {
       danger: '#f1416c',
       dark: '181c32',
     },
+
     extend: {},
+  },
+  // Custom Variants for css classess
+  variants: {
+    animate: ['transition-all', 'duration-300'],
+    display: ['responsive', 'group-hover'],
+    wordBreak: ['responsive', 'group-hover'],
+    textOverflow: ['responsive', 'group-hover'],
+    animation: ['responsive', 'motion-safe', 'motion-reduce'],
+    backgroundColor: ['group-hover', 'hover'],
+    outline: ['focus'],
   },
   plugins: [],
 };
