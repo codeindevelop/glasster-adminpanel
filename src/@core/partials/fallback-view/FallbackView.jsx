ab81641471a1987275d39d09664b259807ffbe4a
codeindevelopment <codeindevelopment@gmail.com>
2022-03-13 03:43:46 +0330

    implement translate for page loading

--- src/@core/partials/fallback-view/FallbackView.jsx
+++ src/@core/partials/fallback-view/FallbackView.jsx
@@ -1,4 +1,5 @@
 import SVG from 'react-inlinesvg';
+import { FormattedMessage } from 'react-intl';
 import Logo from '../../assets/img/logo/logo.svg';
 export function FallbackView() {
   return (
@@ -6,8 +7,12 @@ export function FallbackView() {
       <div className=''>
         <div className=' w-full flex flex-col absolute justify-center h-screen	 items-center  z-10'>
           <SVG src={Logo} alt='Start logo' />
-          <h5 className='font-pop text-slate-500 text-lg text-center mt-2 '>Glasster Website Manager</h5>
-          <h6 className='font-bold text-slate-700 my-2'>در حال بارگذاری ...</h6>
+          <h5 className='font-pop text-slate-500 text-lg text-center mt-2 '>
+            <FormattedMessage id='GLASSTER_LOADING_TEXT' />
+          </h5>
+          <h6 className='font-bold text-slate-700 my-2'>
+            <FormattedMessage id='LOADING_TEXT' />
+          </h6>
         </div>
       </div>
     </>
