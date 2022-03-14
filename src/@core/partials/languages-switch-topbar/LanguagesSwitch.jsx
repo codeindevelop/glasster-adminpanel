bc6adef77fc6b5e66eda65b3f06c2a602d5966ef
codeindevelopment <codeindevelopment@gmail.com>
2022-03-15 03:01:39 +0330

    implement visible effect for user and laguage menu

--- src/@core/partials/languages-switch-topbar/LanguagesSwitch.jsx
+++ src/@core/partials/languages-switch-topbar/LanguagesSwitch.jsx
@@ -8,22 +8,25 @@ export default function LanguagesSwitch() {
   const lang = useLang();
   const currentLanguage = languages.find((x) => x.lang === lang);
 
-  const [showMenu, setShowMenu] = useState('hidden');
+  const [showMenu, setShowMenu] = useState('translate-y-6 opacity-0 invisible');
 
   useEffect(() => {
     document.addEventListener('click', handleClose);
-
   }, [document]);
 
   const handleClose = () => {
-    setShowMenu('hidden');
+    setShowMenu('translate-y-6 opacity-0 invisible');
+  };
+
+  const handleOpen = () => {
+    setShowMenu('translate-y-0 transform ease-in opacity-100 visible');
   };
 
   return (
     <>
       <div className='w-32 h-10 relative transition-all duration-300'>
         <button
-          onMouseEnter={(e) => setShowMenu('flex')}
+          onMouseEnter={(e) => handleOpen()}
           className='flex gap-2 h-full w-full justify-center p-3 items-center dark:hover:bg-white/[.08] hover:bg-slate-100 rounded-lg  w-full  transition-all duration-300   dark:hover:text-primary  '
         >
           <img className='rounded-full w-5 h-5' src={currentLanguage?.flag} alt='glasster-lang' />
@@ -36,6 +39,7 @@ export default function LanguagesSwitch() {
           className={clsx(
             `${showMenu}   absolute top-12 z-10 w-full text-base list-none bg-white rounded  transition-all duration-300  dark:bg-[#1e1e2d] `
           )}
+          onMouseLeave={(e) => handleClose()}
         >
           <ul className='py-1 transition-all duration-300'>
             {languages.map((l) => (
--- src/@core/partials/user-menu/UserMenu.jsx
+++ src/@core/partials/user-menu/UserMenu.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SVG from 'react-inlinesvg';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import Skeleton from '@mui/material/Skeleton';
 
 import blankAvatarPic from 'img/avatar/blank.png';
 import userIcon from 'img/icons/communication/com006.svg';
@@ -11,39 +12,110 @@ import roleIcon from 'img/icons/general/gen019.svg';
 import permissionIcon from 'img/icons/general/gen049.svg';
 import logoutIcon from 'img/icons/technology/teh004.svg';
 import { logoutAction } from 'actions/auth/loginActions';
+import clsx from 'clsx';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
-  const { asideOpen, dir, first_name, last_name, email } = useSelector((state) => ({
+  const [showMenu, setShowMenu] = useState('translate-y-6 opacity-0 invisible');
+  const { asideOpen, dir, first_name, last_name, email, profile_pic } = useSelector((state) => ({
     asideOpen: state.layout.aside.open,
     dir: state.layout.config.direction,
     first_name: state.auth.user.user.first_name,
     last_name: state.auth.user.user.last_name,
     email: state.auth.user.user.email,
+    profile_pic: state.auth.user.user.profile_pic,
   }));
+
+  useEffect(() => {
+    document.addEventListener('click', handleClose);
+  }, [document]);
+
+  const handleClose = () => {
+    setShowMenu('translate-y-6 opacity-0 invisible');
+  };
+
+  const handleOpen = () => {
+    setShowMenu('translate-y-0 transform ease-in opacity-100 visible');
+  };
   return (
     <>
       {/* begin User Menu Wrapper */}
       <div className='relative'>
-        <div className='flex cursor-pointer w-[40px] h-[40px] justify-center items-center rounded-[.475rem]  shadow-md  animation-all duration-300'>
+        <div
+          onMouseEnter={(e) => handleOpen()}
+          className='flex cursor-pointer w-[40px] h-[40px] justify-center items-center rounded-[.475rem]  shadow-md  animation-all duration-300'
+        >
           <img className='w-full h-full rounded-[.475rem]' src={blankAvatarPic} alt='avatar' />
         </div>
         {/* Begin DropDown Menu */}
-        <div className='absolute top-14 left-0 rounded-md shadow-lg border border-1 w-[270px]'>
+        <div
+          className={clsx(
+            `${showMenu} absolute   transition-all duration-200 top-14 left-0 rounded-lg shadow-lg border border-1 dark:border-none dark:bg-[#1e1e2d] w-[270px]`
+          )}
+        >
           {/* Begin Dropdown Wrapper */}
-          <div className='flex flex-col gap-3 divide-y divide-slate-200'>
+          <div
+            onMouseLeave={(e) => handleClose()}
+            className='flex flex-col gap-3 divide-y divide-gray-200 dark:divide-[#2b2b40] w-full'
+          >
             {/* Begin Avatar and info */}
-            <div className='flex gap-5 items-center   px-4 pt-4'>
-              <img
-                className='rounded-[.475rem] w-[50px] h-[50px]'
-                src={blankAvatarPic}
-                alt='avatar'
-              />
-              <div className='flex flex-col items-center gap-1'>
-                <h2 className='text-slate-700 text-center font-bold text-md'>
-                  {first_name} {last_name}
-                </h2>
-                <h6 className='text-slate-500 text-center font-pop text-xs'>{email}</h6>
+            <div className='flex gap-2 items-center   px-4 pt-4 '>
+              {!first_name ? (
+                <>
+                  <Skeleton
+                    sx={{ width: '40px', height: '40px' }}
+                    animation='pulse'
+                    variant='rectangular'
+                    className='rounded-[.475rem]'
+                  />
+                </>
+              ) : (
+                <>
+                  {profile_pic === null ? (
+                    <>
+                      <img
+                        className='rounded-[.475rem] w-[50px] h-[50px]'
+                        src={blankAvatarPic}
+                        alt='avatar'
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        className='rounded-[.475rem] w-[50px] h-[50px]'
+                        src={profile_pic}
+                        alt='avatar'
+                      />
+                    </>
+                  )}
+                </>
+              )}
+
+              <div className='flex flex-col items-center gap-1  w-full'>
+                <div className='w-full'>
+                  {/* Check if redux store not loaded , try to display loading */}
+                  {!first_name ? (
+                    <>
+                      <Skeleton
+                        sx={{ width: '75%', height: '0.6rem' }}
+                        animation='wave'
+                        variant='rectangular'
+                      />
+                      <Skeleton width='100%' />
+                    </>
+                  ) : (
+                    <>
+                      <Link className='group' to='/'>
+                        <h2 className='text-slate-700 group-hover:text-primary dark:text-white  text-center font-bold text-md'>
+                          {first_name} {last_name}
+                        </h2>
+                        <h6 className='text-slate-500 dark:text-white text-center font-pop text-xs'>
+                          {email}
+                        </h6>
+                      </Link>
+                    </>
+                  )}
+                </div>
               </div>
             </div>
             {/* End Avatar and info */}
@@ -53,11 +125,11 @@ export default function UserMenu() {
               <ul className='flex flex-col   py-2'>
                 {/* Begin Profile */}
                 <Link className='group' to='/'>
-                  <li className='group-hover:bg-slate-100 menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
+                  <li className='group-hover:bg-slate-100  dark:group-hover:bg-[#1b1b29cc] menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
                     <span className='svg-icon svg-icon-2 '>
                       <SVG src={userIcon} />
                     </span>
-                    <span className='text-slate-700 w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
+                    <span className='text-slate-700 dark:text-[#cdcdde] w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
                       پروفایل کاربری
                     </span>
                   </li>
@@ -65,11 +137,11 @@ export default function UserMenu() {
                 {/* End Profile */}
                 {/* Begin System Setting */}
                 <Link className='group' to='/'>
-                  <li className='group-hover:bg-slate-100 menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
+                  <li className='group-hover:bg-slate-100 dark:group-hover:bg-[#1b1b29cc] menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
                     <span className='svg-icon svg-icon-2 '>
                       <SVG src={settingsIcon} />
                     </span>
-                    <span className='text-slate-700 w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
+                    <span className='text-slate-700 dark:text-[#cdcdde] w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
                       پیکربندی سامانه
                     </span>
                   </li>
@@ -77,11 +149,11 @@ export default function UserMenu() {
                 {/* End System Setting */}
                 {/* Begin Users List */}
                 <Link className='group' to='/'>
-                  <li className='group-hover:bg-slate-100 menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
+                  <li className='group-hover:bg-slate-100 dark:group-hover:bg-[#1b1b29cc] menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
                     <span className='svg-icon svg-icon-2 '>
                       <SVG src={usersIcon} />
                     </span>
-                    <span className='text-slate-700 w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
+                    <span className='text-slate-700 dark:text-[#cdcdde] w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
                       کاربران سامانه
                     </span>
                   </li>
@@ -89,11 +161,11 @@ export default function UserMenu() {
                 {/* End Users List */}
                 {/* Begin Roles Settings */}
                 <Link className='group' to='/'>
-                  <li className='group-hover:bg-slate-100 menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
+                  <li className='group-hover:bg-slate-100 dark:group-hover:bg-[#1b1b29cc] menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
                     <span className='svg-icon svg-icon-2 '>
                       <SVG src={roleIcon} />
                     </span>
-                    <span className='text-slate-700 w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
+                    <span className='text-slate-700 dark:text-[#cdcdde] w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
                       مدیریت نقش ها
                     </span>
                   </li>
@@ -101,11 +173,11 @@ export default function UserMenu() {
                 {/* End Roles Settings */}
                 {/* Begin Roles Settings */}
                 <Link className='group' to='/'>
-                  <li className='group-hover:bg-slate-100 menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
+                  <li className='group-hover:bg-slate-100 dark:group-hover:bg-[#1b1b29cc] menu-icon gap-5 py-2 group px-3 mx-3 rounded-lg cursor-pointer flex items-center  '>
                     <span className='svg-icon svg-icon-2 '>
                       <SVG src={permissionIcon} />
                     </span>
-                    <span className='text-slate-700 w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
+                    <span className='text-slate-700 dark:text-[#cdcdde] w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
                       مدیریت سطوح دسترسی
                     </span>
                   </li>
@@ -117,11 +189,11 @@ export default function UserMenu() {
 
             {/* Begin Signout */}
             <ul onClick={(e) => dispatch(logoutAction())} className='  py-2 group'>
-              <li className='group-hover:bg-slate-100 flex flex-row items-center gap-4 py-2 group px-3 mx-3 rounded-lg cursor-pointer '>
+              <li className='group-hover:bg-slate-100 dark:group-hover:bg-[#1b1b29cc] flex flex-row items-center gap-4 py-2 group px-3 mx-3 rounded-lg cursor-pointer '>
                 <span className='svg-icon svg-icon-2 '>
                   <SVG src={logoutIcon} />
                 </span>
-                <span className='text-slate-700 w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
+                <span className='text-slate-700 dark:text-[#cdcdde] w-full h-full text-sm font-light group-hover:text-primary 	 group-hover:mx-1 transition-all duration-300'>
                   خروج از سامانه
                 </span>
               </li>
